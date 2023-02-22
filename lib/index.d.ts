2beab70d4dba66b42b45524fa7e2308434d5b8b8
st.dremin@ya.ru <stdremin>
2023-02-22 21:08:44 +0300

    v1.0.0

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,5 @@
 import { BaseEntity } from "./BaseEntity";
-export { BaseEntity };
+import { BaseEntityType, BaseCastType, Cast, Relation, Collection, ModelDecorator } from "./Decorators";
+import { CastAttribute } from "./Casts/CastAttribute";
+export { BaseEntity, Relation, Cast, Collection, ModelDecorator, CastAttribute };
+export type { BaseEntityType, BaseCastType };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { BaseEntity } from "./BaseEntity";
+import { BaseEntityType, BaseCastType, Cast, Relation, Collection, ModelDecorator } from "./Decorators";
+import { CastAttribute } from "./Casts/CastAttribute";
 
-export { BaseEntity };
+export { BaseEntity, Relation, Cast, Collection, ModelDecorator, CastAttribute };
+export type { BaseEntityType, BaseCastType };
+
